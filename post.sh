54812c1700dcac54acb5af846cb69d6c23a662d1
redesgtt <69505347+redesgtt@users.noreply.github.com>
2020-09-17 18:46:31 +0200

    Update post.sh

--- post.sh
+++ post.sh
@@ -3,4 +3,4 @@
 
 echo "-Z root -w ./round_time%S.cap -G $POST -z ./bcast_AI_v15.py >> $FILENAME.csv"
 
-tcpdump -ni $IFACE2  -Z root -w ./round_time%S.cap -G $POST -z ./bcast_ids_lite_v1.py >> $FILENAME.csv
+tcpdump -ni $IFACE2  -Z root -w ./round_time%S.cap -G $POST -z ./bcast_ids_lite_v2.py >> $FILENAME.csv
