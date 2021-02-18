cce4fc7f2942eccf706810a5e204f2519edc39a1
root <redes@gtt.es>
2021-02-18 07:41:08 -0500

    ICMPv6 Parameter Problem

--- post.sh
+++ post.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . ./config.txt
 
-MAC=`/sbin/ifconfig $IFACE2 | grep ether | awk '{print $2}'`
+#MAC=`/sbin/ifconfig $IFACE2 | grep ether | awk '{print $2}'`
 
-echo "tcpdump -ni $IFACE2  -Z root -w ./round_time%S.cap -G $POST -z ./bcast_ids_lite_v2.py not ether src host $MAC >> $FILENAME.csv"
+echo "tcpdump -ni $IFACE2  -Z root -w ./round_time%S.cap -G $POST -z ./bcast_ids_lite_v3.py >> $FILENAME.csv"
 
-tcpdump -ni $IFACE2  -Z root -w ./round_time%S.cap -G $POST -z ./bcast_ids_lite_v2.py "not ether src host $MAC" >> $FILENAME.csv
+tcpdump -ni $IFACE2  -Z root -w ./round_time%S.cap -G $POST -z ./bcast_ids_lite_v3.py >> $FILENAME.csv
